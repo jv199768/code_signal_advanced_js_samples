887c70657516f9f618baa0e60bcf81fc36e037a6
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 23:10:50 -0400

    Create shapes.js

---
+++ shapes.js
@@ -0,0 +1,8 @@
+// The content of shapes.js
+export function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+export function calculateTriangleArea(base, height) {
+    return 0.5 * base * height;
+}
