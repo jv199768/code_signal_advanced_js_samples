7ac2de259264deba736b68ae7813b14018ec7b03
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 22:44:12 -0400

    Create person.js

---
+++ person.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let person = new Person("John Doe", 30);
+console.log(person.name);  // Outputs "John Doe"
+console.log(person.age);   // Outputs 30
